61de4bf18c16c9e621ea94ad1839dc613ccab42b
ghostbuster91 <kghost0@gmail.com>
2017-10-04 21:45:47 +0200

    Init repo

---
+++ .setup.sh
@@ -0,0 +1,4 @@
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
