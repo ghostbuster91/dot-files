1d14b80b33db35dd401464baa40178b9280bbd1b
ghostbuster91 <kghost0@gmail.com>
2017-10-05 07:41:08 +0200

    Swtich to zsh

--- .setup.sh
+++ .setup.sh
@@ -2,3 +2,4 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
 source ~/.zsh/zsh-autosuggestions/zsh-autosuggestions.zsh
+chsh -s `which zsh`
