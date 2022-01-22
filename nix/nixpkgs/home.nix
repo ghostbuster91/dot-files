68c98625b70455f78a9a2734f72dd746303194a1
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2022-01-22 13:27:57 +0100

    Git: Extract config to a separate file

--- nix/nixpkgs/home.nix
+++ nix/nixpkgs/home.nix
@@ -40,6 +40,7 @@
     ./programs/tmux
     ./programs/zsh
     ./programs/neovim
+    ./programs/git
   ];
 
   home.packages = with pkgs; [
@@ -82,38 +83,6 @@
     xsel # for tmux-yank
   ];
 
-  programs.git = {
-    enable = true;
-    userName = "ghostbuster91";
-    userEmail = "ghostbuster91@users.noreply.github.com";
-    extraConfig = {
-      merge = { conflictStyle = "diff3"; };
-      core = {
-        editor = "nvim";
-        pager = "diff-so-fancy | less -FXRi";
-      };
-      color = { ui = true; };
-      push = { default = "simple"; };
-      pull = { ff = "only"; };
-      init = { defaultBranch = "main"; };
-      alias = { gone = "!bash ~/bin/git-gone.sh"; };
-    };
-    includes = [
-      {
-        condition = "gitdir:~/dev/";
-        contents = {
-          user = {
-            name = "Kasper Kondzielski";
-            email = "kasper.kondzielski@iohk.io";
-            signingKey = "5A22DAA4D02BA94A";
-          };
-          commit = {
-            gpgSign = true;
-          };
-        };
-      }
-    ];
-  };
 
   programs.fzf = {
     enable = true;
