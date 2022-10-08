0232f286738975d03f1ed9a66d1484c640f8e01b
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2022-10-08 19:43:24 +0200

    Update flake inputs

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Home Manager configuration of Jane Doe";
+  description = "Home Manager configuration of Kasper Kondzielski";
 
   inputs = {
     nix.url = "github:nixos/nix/2.9-maintenance";
@@ -14,29 +14,36 @@
     };
   };
 
-  outputs = inputs @ { home-manager, ... }:
+  outputs = inputs @ { home-manager, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       username = "kghost";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+        overlays = [
+          (self: super: { alacritty = import ./overlays/alacritty.nix { nixGL = inputs.nixGL; pkgs = super; }; })
+          (self: super: { derivations = import ./derivations { pkgs = super; }; })
+        ];
+      };
     in
     {
       homeConfigurations.${username} = home-manager.lib.homeManagerConfiguration {
-        # Specify the path to your home configuration here
-        configuration = import ./home.nix;
-
+        inherit pkgs;
         extraSpecialArgs = {
           inherit inputs;
         };
 
-        inherit system username;
-        homeDirectory = "/home/${username}";
-        # Update the state version as needed.
-        # See the changelog here:
-        # https://nix-community.github.io/home-manager/release-notes.html#sec-release-21.05
-        stateVersion = "22.05";
-
-        # Optionally use extraSpecialArgs
-        # to pass through arguments to home.nix
+        modules = [
+          ./home.nix
+          {
+            home = {
+              inherit username;
+              homeDirectory = "/home/${username}";
+              stateVersion = "22.05";
+            };
+          }
+        ];
       };
     };
 }
