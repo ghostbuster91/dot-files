20213c6d487c6e6b71f5043cbf6ccedb89e2ab72
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2022-10-30 16:33:58 +0100

    [Neovim] Update neogit to the latest version

--- derivations/nvim-neogit.nix
+++ derivations/nvim-neogit.nix
@@ -1,12 +1,12 @@
 { pkgs }:
 pkgs.vimUtils.buildVimPluginFrom2Nix {
   pname = "neogit";
-  version = "74c9e29b61780345d3ad9d7a4a4437607caead4a";
+  version = "1acb13c07b34622fe1054695afcecff537d9a00a";
   src = pkgs.fetchFromGitHub {
     owner = "TimUntersberger";
     repo = "neogit";
-    rev = "74c9e29b61780345d3ad9d7a4a4437607caead4a";
-    sha256 = "sha256-D08s/Nwsbyqgl2ODuSyD2uVyQdR+rwj6hciOLVWSXx8=";
+    rev = "1acb13c07b34622fe1054695afcecff537d9a00a";
+    sha256 = "sha256-XBFR5ancD75Ey6AGlgVjqWEEJQpNc2G/6mp3iB2fMPI=";
   };
   meta.homepage = "https://github.com/TimUntersberger/neogit/";
 }
