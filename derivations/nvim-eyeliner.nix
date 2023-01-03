0993e6e9aa48c626f2d5c755b5df32ae68dec351
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-01-03 19:41:38 +0100

    [Nvim] Update eyeliner

--- derivations/nvim-eyeliner.nix
+++ derivations/nvim-eyeliner.nix
@@ -1,11 +1,11 @@
 { pkgs }:
-pkgs.vimUtils.buildVimPluginFrom2Nix {
+pkgs.vimUtils.buildVimPluginFrom2Nix rec {
   pname = "nvim-eyeliner";
-  version = "38e090adb5f56d6c06000a8bfdf3098554d7b784";
+  version = "353e7b2f517953c48608ee158ebd161ca5b6cfae";
   src = pkgs.fetchFromGitHub {
     owner = "jinh0";
     repo = "eyeliner.nvim";
-    rev = "38e090adb5f56d6c06000a8bfdf3098554d7b784";
-    sha256 = "sha256-56fGrXcZAfell5SbMtCVfmxNoJkMM5FmXnuO110HkyM=";
+    rev = version;
+    sha256 = "sha256-XBikd1Io25NkeN00bWBf63zbGQSWpLObH/bWBvlYFZE=";
   };
 }
