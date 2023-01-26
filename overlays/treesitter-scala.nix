139eb4ff763da136aeae4e12bffcc6eb2def0120
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-01-26 22:27:29 +0100

    chore: Upodate nixpkgs with treesitter-scala

--- overlays/treesitter-scala.nix
+++ overlays/treesitter-scala.nix
@@ -2,7 +2,7 @@
 pkgs.fetchFromGitHub {
   owner = "tree-sitter";
   repo = "tree-sitter-scala";
-  rev = "94bb82c3aa68b08cf23f7458e75adcd558c40bef";
-  sha256 = "sha256-xK9l1eIiTef1NwAKuzSOzQF5sSsEqPk+gxkULxCakhY=";
+  rev = "40cb5fc38c5b58441faa2afb78d3ede730c9c206";
+  sha256 = "sha256-kwoDs7zPwZH93bmlEjhlPf0l9le4whEDmQtcIyPK1XQ=";
 }
 
