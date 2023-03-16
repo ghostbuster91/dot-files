81c7a06ee6686762bfd7f3d73dabbdd20b97f815
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-03-16 18:44:16 +0100

    [Nix] Update metals to 0.11.11

--- overlays/metalsOverlay.nix
+++ overlays/metalsOverlay.nix
@@ -3,6 +3,6 @@ let
   metalsBuilder = import ./metalsBuilder.nix { inherit pkgs; };
 in
 metalsBuilder {
-  version = "0.11.8";
-  outputHash = "sha256-j7je+ZBTIkRfOPpUWbwz4JR06KprMn8sZXONrtI/n8s=";
+  version = "0.11.11";
+  outputHash = "sha256-oz4lrRnpVzc9kN+iJv+mtV/S1wdMKwJBkKpvmWCSwE0=";
 }
