bccbc13356ef08a0d6c939590ccfebaf782de25c
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-03-29 17:39:07 +0200

    [Nvim] Update treesitter-scala

--- overlays/treesitter-scala.nix
+++ overlays/treesitter-scala.nix
@@ -2,7 +2,7 @@
 pkgs.fetchFromGitHub {
   owner = "tree-sitter";
   repo = "tree-sitter-scala";
-  rev = "40cb5fc38c5b58441faa2afb78d3ede730c9c206";
-  sha256 = "sha256-kwoDs7zPwZH93bmlEjhlPf0l9le4whEDmQtcIyPK1XQ=";
+  rev = "7d348f51e442563f4ab2b6c3e136dac658649f93";
+  hash = "sha256-jIbVw4jKMJYbKeeai3u7J+xKRfo2YNoL3ZcW1NLc9fg=";
 }
 
