2d22944132d238857c40ea8e3a37f731784fc735
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-04-07 23:23:27 +0200

    [Nvim] Update metals

--- derivations/nvim-metals.nix
+++ derivations/nvim-metals.nix
@@ -1,11 +1,11 @@
 { pkgs }:
-pkgs.vimUtils.buildVimPluginFrom2Nix {
+pkgs.vimUtils.buildVimPluginFrom2Nix rec {
   pname = "nvim-metals";
-  version = "1071ddc47e9d9629ba754ffbf3cc11fa8be4218b";
+  version = "51e88e4f5eeadbd92a75cae71c5cbb75f3cb6765";
   src = pkgs.fetchFromGitHub {
     owner = "scalameta";
     repo = "nvim-metals";
-    rev = "51e88e4f5eeadbd92a75cae71c5cbb75f3cb6765";
+    rev = version;
     hash = "sha256-wvpXroM9U4WPv9WaDfT99H9JlHRrIlZiothuLhJtBFM=";
   };
   meta.homepage = "https://github.com/scalameta/nvim-metals/";
