837d127ad806d59d1db573609fa92b0cf2a351f9
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-05-07 08:50:09 +0200

    [NixOS] Switch to tlp power management based profiles
    This will make it easier to transition away from gnome

--- machines/focus/configuration.nix
+++ machines/focus/configuration.nix
@@ -186,6 +186,16 @@
       polkitPolicyOwners = [ username ];
     };
   };
+
+  # power management features
+  services.power-profiles-daemon.enable = false;
+  services.tlp.enable = true;
+  services.tlp.settings = {
+    CPU_SCALING_GOVERNOR_ON_AC = "performance";
+    CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+    CPU_HWP_ON_AC = "performance";
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It’s perfectly fine and recommended to leave
