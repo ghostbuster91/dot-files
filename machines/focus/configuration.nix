67cdf2686ba9ac80bbf4ab6e91806fe4aff136d5
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-05-19 22:20:13 +0200

    [NixOS] Display list of changes on rebuild

--- machines/focus/configuration.nix
+++ machines/focus/configuration.nix
@@ -233,5 +233,10 @@ in
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "22.11"; # Did you read the comment?
 
+  system.activationScripts.diff = ''
+    if [[ -e /run/current-system ]]; then
+      ${pkgs.nix}/bin/nix store diff-closures /run/current-system "$systemConfig"
+    fi
+  '';
 }
 
