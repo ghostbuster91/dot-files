dd5580dcdcb4ffd35537c4311f08aa4fead44113
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-05-23 12:43:00 +0200

    [NixOS] Configure docker

--- machines/focus/configuration.nix
+++ machines/focus/configuration.nix
@@ -213,7 +213,23 @@ in
     };
   };
 
-  virtualisation.docker.enable = true;
+  virtualisation = {
+    docker = {
+      enable = true;
+      autoPrune = {
+        enable = true;
+        dates = "weekly";
+      };
+      daemon.settings = {
+        bip = "169.254.0.1/16";
+      };
+    };
+
+    virtualbox.host = {
+      enable = false;
+      enableExtensionPack = false;
+    };
+  };
   users.extraGroups.docker.members = [ username ];
   # virtualisation.docker.rootless = {
   #   enable = true;
