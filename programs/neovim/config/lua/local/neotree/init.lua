b0f914f05af52172eb5b27aee5e0111336adee87
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-07-06 23:25:44 +0200

    Don't hide git ignored by default

--- programs/neovim/config/lua/local/neotree/init.lua
+++ programs/neovim/config/lua/local/neotree/init.lua
@@ -253,7 +253,7 @@ local setup = function()
             filtered_items = {
                 --These filters are applied to both browsing and searching
                 hide_dotfiles = false,
-                hide_gitignored = true,
+                hide_gitignored = false,
                 never_show = {
                     ".git",
                 },
