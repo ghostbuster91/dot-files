14dd8bd47303f76caaef94af6e5d45340c3c9789
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-09-19 09:36:19 +0200

    [Nix](chore) Rewrite boot configuration

--- machines/focus/configuration.nix
+++ machines/focus/configuration.nix
@@ -21,20 +21,17 @@ in
       ./cache.nix
     ];
 
-  # Use the GRUB 2 boot loader.
-  # boot.loader.grub.enable = true;
-  # boot.loader.grub.version = 2;
-  # boot.loader.grub.enableCryptodisk = true;
-  # boot.loader.grub.efiSupport = true;
-  # boot.loader.grub.efiInstallAsRemovable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader.efi.efiSysMountPoint = "/boot";
-  # Define on which hard drive you want to install Grub.
-  # boot.loader.grub.device = "nodev"; # or "nodev" for efi only
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.systemd-boot.configurationLimit = 20;
-
-
+  boot.loader =
+    {
+      systemd-boot = {
+        enable = true;
+        configurationLimit = 20;
+      };
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+    };
 
   networking = {
     hostId = "d1084363";
