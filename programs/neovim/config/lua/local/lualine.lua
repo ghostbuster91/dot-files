58a957d0b583d6beeb46408f35dc54b0f5b972f0
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2023-11-23 22:09:50 +0100

    [Nvim] Add macro recording indicator to lualine

--- programs/neovim/config/lua/local/lualine.lua
+++ programs/neovim/config/lua/local/lualine.lua
@@ -1,5 +1,44 @@
 local setup = function(navic)
-    require("lualine").setup({
+    local lualine = require("lualine")
+
+    -- https://www.reddit.com/r/neovim/comments/xy0tu1/comment/irfegvd/?context=3
+    local function show_macro_recording()
+        local recording_register = vim.fn.reg_recording()
+        if recording_register == "" then
+            return ""
+        else
+            return "Recording @" .. recording_register
+        end
+    end
+    vim.api.nvim_create_autocmd("RecordingEnter", {
+        callback = function()
+            lualine.refresh({
+                place = { "statusline" },
+            })
+        end,
+    })
+
+    vim.api.nvim_create_autocmd("RecordingLeave", {
+        callback = function()
+            -- This is going to seem really weird!
+            -- Instead of just calling refresh we need to wait a moment because of the nature of
+            -- `vim.fn.reg_recording`. If we tell lualine to refresh right now it actually will
+            -- still show a recording occuring because `vim.fn.reg_recording` hasn't emptied yet.
+            -- So what we need to do is wait a tiny amount of time (in this instance 50 ms) to
+            -- ensure `vim.fn.reg_recording` is purged before asking lualine to refresh.
+            local timer = vim.loop.new_timer()
+            timer:start(
+                50,
+                0,
+                vim.schedule_wrap(function()
+                    lualine.refresh({
+                        place = { "statusline" },
+                    })
+                end)
+            )
+        end,
+    })
+    lualine.setup({
         options = {
             icons_enabled = true,
             theme = "auto",
@@ -11,7 +50,15 @@ local setup = function(navic)
         },
         sections = {
             lualine_a = { "mode" },
-            lualine_b = { "branch", "diff", "diagnostics" },
+            lualine_b = {
+                "branch",
+                "diff",
+                "diagnostics",
+                {
+                    "macro-recording",
+                    fmt = show_macro_recording,
+                },
+            },
             lualine_c = {
                 'filename',
                 {
