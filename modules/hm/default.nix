9c2c0f68d003f9ef2589c0b0e7a4f779009e3f9d
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2024-12-10 01:19:27 +0100

    Restore HM

--- modules/hm/default.nix
+++ modules/hm/default.nix
@@ -1,4 +1,4 @@
-_:
+{ self, inputs, ... }:
 {
   flake.homeModules = {
     base = ./home.nix;
@@ -10,4 +10,40 @@ _:
     scala = ./scala;
     foot = ./foot;
   };
+
+  flake.homeConfigurations =
+    let
+      username = "kghost";
+      system = "x86_64-linux";
+      languageServers = {
+        inherit (inputs.nix-metals.packages.${system}) metals;
+        inherit (inputs.nix-smithy-ls.packages.${system}) disney-smithy-ls;
+      };
+      pkgs-unstable = (import inputs.nixpkgs-unstable {
+        inherit system;
+        overlays = [ self.overlays.default ];
+        config.allowUnfree = true;
+      }) // languageServers;
+
+      pkgs-stable = (import inputs.nixpkgs {
+        inherit system;
+        overlays = [ self.overlays.default ];
+        config.allowUnfree = true;
+      }) // languageServers;
+    in
+    {
+      focus = inputs.home-manager.lib.homeManagerConfiguration {
+        extraSpecialArgs = { inherit username; inherit pkgs-unstable; inherit pkgs-stable; };
+        pkgs = pkgs-stable;
+        modules = [
+          self.homeModules.base
+          self.homeModules.nvim
+          self.homeModules.git
+          self.homeModules.zsh
+          self.homeModules.tmux
+          self.homeModules.alacritty
+          self.homeModules.scala
+        ];
+      };
+    };
 }
