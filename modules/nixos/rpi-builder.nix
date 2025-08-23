1f886c77245404d16c1b232faf48a083cb0ab6db
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2025-08-23 15:22:10 +0200

    Disable builders-use-substitutes for rpi-builder
    This caused the target system to require internet access

--- modules/nixos/rpi-builder.nix
+++ modules/nixos/rpi-builder.nix
@@ -34,6 +34,6 @@ _:
   nix.distributedBuilds = true;
   # optional, useful when the builder has a faster internet connection than yours
   nix.extraOptions = ''
-    builders-use-substitutes = true
+    builders-use-substitutes = false
   '';
 }
