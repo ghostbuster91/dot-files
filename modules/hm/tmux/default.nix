f7a8103a355853d9372d2230438629fefd3ba057
ghostbuster91 <ghostbuster91@users.noreply.github.com>
2026-05-03 16:41:50 +0200

    Increase tmux buffer size

--- modules/hm/tmux/default.nix
+++ modules/hm/tmux/default.nix
@@ -126,7 +126,7 @@ in
       onedark_white="#aab2bf"
       set -g status-right "#[fg=$onedark_white,bg=$onedark_black] #(${pkgs-unstable.tmux-mem-cpu-load}/bin/tmux-mem-cpu-load -g 5 --interval 2) #[fg=$onedark_green,bg=$onedark_black]"
 
-      set-option -g history-limit 5000
+      set-option -g history-limit 100000
     '';
   };
 }
